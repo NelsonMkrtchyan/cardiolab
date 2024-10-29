661f1e0a3d94b9a0eecd445c2ae42625bb7bd3cd
nelsonmkrtchyan <nelsmkrtchbyan@gmail.com>
2024-10-29 23:29:00 +0400

    tailwind and next config changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,61 @@ import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
-  content: ["./src/**/*.tsx"],
+  // content: ["./src/**/*.tsx"],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  darkMode: "class",
   theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    screens: {
+      xs: "450px",
+      // => @media (min-width: 450px) { ... }
+
+      sm: "575px",
+      // => @media (min-width: 576px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "992px",
+      // => @media (min-width: 992px) { ... }
+
+      xl: "1200px",
+      // => @media (min-width: 1200px) { ... }
+
+      "2xl": "1400px",
+      // => @media (min-width: 1400px) { ... }
+    },
     extend: {
+      colors: {
+        current: "currentColor",
+        transparent: "transparent",
+        white: "#FFFFFF",
+        black: "#121723",
+        dark: "#1D2430",
+        primary: "#4A6CF7",
+        yellow: "#FBB040",
+        "bg-color-dark": "#171C28",
+        "body-color": {
+          DEFAULT: "#788293",
+          dark: "#959CB1",
+        },
+        stroke: {
+          stroke: "#E3E8EF",
+          dark: "#353943",
+        },
+        gray: {
+          // ...colors.gray,
+          dark: "#1E232E",
+          light: "#F0F2F9",
+        },
+      },
       fontFamily: {
         sans: ["var(--font-geist-sans)", ...fontFamily.sans],
       },
