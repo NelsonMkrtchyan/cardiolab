3117dbca0fce11427ce29af590687bef27c38e1f
nelsonmkrtchyan <nelsmkrtchbyan@gmail.com>
2024-11-11 21:10:42 +0400

    integrate next-intl and i18, config routing to handle locales and changed favicon

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,60 @@
+import "~/styles/globals.css";
+
+import { GeistSans } from "geist/font/sans";
+import { type Metadata } from "next";
+import React from "react";
+import ScrollToTop from "~/app/_components/ScrollToTop";
+import NavigationBar from "~/app/_components/NavigationBar";
+import { Providers } from "~/app/providers";
+import Footer from "~/app/_components/Footer";
+
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+
+import { notFound } from "next/navigation";
+import { routing } from "~/i18n/routing";
+
+export const metadata: Metadata = {
+  title: "CardioLab",
+  description: "CardioLab",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+export default async function RootLayout({
+  children,
+  params: { locale },
+}: Readonly<{
+  children: React.ReactNode;
+  params: { locale: string };
+}>) {
+  // Ensure that the incoming `locale` is valid
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  if (!routing.locales.includes(locale)) {
+    console.log("Should be not found");
+    notFound();
+  }
+
+  // Providing all messages to the client
+  // side is the easiest way to get started
+  const messages = await getMessages();
+
+  return (
+    <html suppressHydrationWarning lang="en">
+      <body
+        className={`bg-[#FCFCFC] dark:bg-black ${GeistSans.variable} flex flex-col`}
+      >
+        <Providers>
+          <NextIntlClientProvider messages={messages}>
+            <div className="flex h-screen w-full flex-col justify-between">
+              <NavigationBar />
+              <div className="w-full">{children}</div>
+              <Footer />
+              <ScrollToTop />
+            </div>
+          </NextIntlClientProvider>
+        </Providers>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,43 @@
+import { db } from "~/server/db";
+import React from "react";
+import SectionPlaceholder from "~/app/_components/SectionPlaceholder";
+import { Video } from "~/app/_components/Video";
+import { SectionTitle } from "~/app/_components/SectionTitle";
+import { benefitOne, Benefits, benefitTwo } from "~/app/_components/Benefits";
+
+export const dynamic = "force-dynamic";
+
+const HomePage = async () => {
+  const posts = await db.query.posts.findMany();
+  console.log("posts", posts);
+
+  return (
+    <>
+      <SectionPlaceholder />
+
+      {/*#############*/}
+      <SectionTitle
+        preTitle="Watch a video"
+        title="Learn how to fullfil your needs"
+      >
+        This section is to highlight a promo or demo video of your product.
+        Analysts says a landing page with video has 3% more conversion rate. So,
+        don&apos;t forget to add one. Just like this.
+      </SectionTitle>
+      <Video videoId={"znyBJC7NDOk?si=X5dZoiD2cHbFcXdT"} />
+
+      {/*#############*/}
+
+      {/*#############*/}
+      <Benefits data={benefitOne} />
+
+      <Benefits imgPos="right" data={benefitTwo} />
+
+      {/*#############*/}
+
+      <SectionPlaceholder />
+    </>
+  );
+};
+
+export default HomePage;
