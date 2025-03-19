b3320ee3229ea7b0435e5207ceb149aa1d8ef49f
nelsonmkrtchyan <nelmkrtchyan@gmail.com>
2025-03-19 16:06:04 +0400

    make doctors cards non-clickable

--- src/app/[locale]/stuff/_components/Card.tsx
+++ src/app/[locale]/stuff/_components/Card.tsx
@@ -1,28 +1,34 @@
 import Image from "next/image";
 import React from "react";
 import { type CardI } from "~/constants/stuff";
-import { Link } from "~/i18n/routing";
+// import { Link } from "~/i18n/routing";
 
 const Card = ({ employee }: CardI) => {
-  const { id, name, image, position } = employee;
+  const {
+    // id,
+    name,
+    image,
+    position,
+  } = employee;
   return (
     <>
       <div className="col-sm-6 col-lg-4">
-        <Link href={`/stuff/details/${id}`}>
-          <div className="doctor-item">
-            <div className="doctor-top">
-              <Image src={image} alt="Doctor" width={364} height={393} />
-
-              {/*<Link href="/appointment">Get Appointment</Link>*/}
-            </div>
-            <div className="doctor-bottom">
-              <h3>
-                <Link href={`/stuff/details/${id}`}>{name}</Link>
-              </h3>
-              <span>{position}</span>
-            </div>
+        {/*<Link href={`/stuff/details/${id}`}>*/}
+        <div className="doctor-item">
+          <div className="doctor-top">
+            <Image src={image} alt="Doctor" width={364} height={393} />
+            {/*TODO: Uncomment in the future*/}
+            {/*<Link href="/appointment">Get Appointment</Link>*/}
           </div>
-        </Link>
+          <div className="doctor-bottom">
+            <h3>
+              {/*<Link href={`/stuff/details/${id}`}>{name}</Link>*/}
+              {name}
+            </h3>
+            <span>{position}</span>
+          </div>
+        </div>
+        {/*</Link>*/}
       </div>
     </>
   );
