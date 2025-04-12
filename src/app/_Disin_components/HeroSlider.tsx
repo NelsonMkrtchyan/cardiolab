6b5dc718b8ea3cb93f4f54c1e1e29e3509700365
nelsonmkrtchyan <nelmkrtchyan@gmail.com>
2025-04-12 23:06:14 +0400

    add second slide and link to prcelist

--- src/app/_Disin_components/HeroSlider.tsx
+++ src/app/_Disin_components/HeroSlider.tsx
@@ -4,11 +4,11 @@ import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Navigation } from "swiper/modules";
 import { useTranslations } from "next-intl";
-// import { Link } from "~/i18n/routing";
+import { Link } from "~/i18n/routing";
 
 const HeroSlider = () => {
-  // const t = useTranslations("HeroSlider");
-  // const tAbout = useTranslations("About");
+  const t = useTranslations("HeroSlider");
+  const tAbout = useTranslations("About");
 
   return (
     <>
@@ -32,16 +32,6 @@ const HeroSlider = () => {
               <div className="d-table-cell">
                 <div className="container">
                   <div className="slider-text">
-                    {/*TODO: From template, not using now*/}
-                    {/*<div className="slider-shape">*/}
-                    {/*  <img*/}
-                    {/*    src="/images/home-one/home-slider1.png"*/}
-                    {/*    alt="Shape"*/}
-                    {/*    width={730}*/}
-                    {/*    height={500}*/}
-                    {/*  />*/}
-                    {/*</div>*/}
-
                     {/*<h1>{t("descriptionOne")}</h1>*/}
                     {/*<p>{tAbout("Mission")}</p>*/}
 
@@ -62,35 +52,27 @@ const HeroSlider = () => {
         {/*/!*TODO: From template, not using now*!/*/}
         <SwiperSlide>
           <div
-            className="slider-item"
+            className="slider-item blur"
             style={{
               backgroundImage: `url(https://x41q9wll8l.ufs.sh/f/kPqN7718CWlun2PT6Vz8iXtIHULns6k3RoJDbzQgEqF9Vf7m)`,
             }}
-          >
-            <div className="d-table">
-              <div className="d-table-cell">
-                <div className="container">
-                  <div className="slider-text">
-                    {/*TODO: From template, not using now*/}
-                    {/*<div className="slider-shape">*/}
-                    {/*  <img*/}
-                    {/*    src="/images/home-one/home-slider1.png"*/}
-                    {/*    alt="Shape"*/}
-                    {/*    width={730}*/}
-                    {/*    height={500}*/}
-                    {/*  />*/}
-                    {/*</div>*/}
+          />
+          <div className="d-table slider-item-content-alternative">
+            <div className="d-table-cell">
+              <div className="container">
+                <div className="slider-text">
+                  <h1>{t("priceTitle")}</h1>
+                  {/*<p>{tAbout("Mission")}</p>*/}
 
-                    {/*<h1>{t("descriptionOne")}</h1>*/}
-                    {/*<p>{tAbout("Mission")}</p>*/}
-
-                    {/*TODO: From template, not using now*/}
-                    {/*<div className="common-btn">*/}
-                    {/*  <Link href="/appointment">Get Appointment</Link>*/}
-                    {/*  <Link href="/about" className="cmn-btn-right">*/}
-                    {/*    Learn More*/}
-                    {/*  </Link>*/}
-                    {/*</div>*/}
+                  {/*TODO: From template, not using now*/}
+                  <div className="common-btn">
+                    {/*<Link href="/appointment">Get Appointment</Link>*/}
+                    <Link
+                      href="/patientGuide/pricelist"
+                      className="cmn-btn-right"
+                    >
+                      Learn More
+                    </Link>
                   </div>
                 </div>
               </div>
