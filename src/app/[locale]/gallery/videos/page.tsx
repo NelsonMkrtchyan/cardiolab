83daecfc625f21ea01041841ba716644ebaeeb6d
nelsonmkrtchyan <nelmkrtchyan@gmail.com>
2025-04-24 11:34:24 +0400

    fix issue with import

--- src/app/[locale]/gallery/videos/page.tsx
+++ src/app/[locale]/gallery/videos/page.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import PageBanner from "~/app/_Disin_components/common/PageBanner";
 import { useTranslations } from "next-intl";
-import PodcastsCards from "~/app/[locale]/podcasts/_components/PodcastsCards";
 import ImagesDB from "~/constants/ImageDatabase.json";
+import PodcastsCards from "~/app/[locale]/gallery/podcasts/_components/PodcastsCards";
 
 export default function Page() {
   const tMenu = useTranslations("Menu");
@@ -14,10 +14,6 @@ export default function Page() {
         homePageUrl="/gallery"
         homePageText={tMenu("Gallery.Gallery")}
         activePageText={tMenu("Gallery.Videos")}
-        // bgImage={`${ImagesDB.AbstractImages.abstract_one}`}
-        // bgImage={`${ImagesDB.AbstractImages.abstract_two}`}
-        // bgImage={`${ImagesDB.AbstractImages.abstract_three}`}
-        // bgImage={`${ImagesDB.AbstractImages.abstract_four}`}
         bgImage={`${ImagesDB.AbstractImages.abstract_five}`}
       />
       <PodcastsCards />
